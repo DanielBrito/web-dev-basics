8a2678c6636b3bc89c0b5f79dd1e9b891d12a16d
danielbrito <daniell-henrique@hotmail.com>
2021-05-10 22:21:28 -0300

    Learning Gulp

---
+++ Browsersync & Sass & Gulp/Project/gulpfile.js
@@ -0,0 +1,52 @@
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync").create();
+
+// Sass Task
+function scssTask() {
+  return src("app/scss/style.scss", { sourcemaps: true })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+exports.scssTask = scssTask;
+
+// JavaScript Task
+function jsTask() {
+  return src("app/js/script.js", { sourcemaps: true })
+    .pipe(terser())
+    .pipe(dest("dist", { sourcemaps: "." }));
+}
+
+exports.jsTask = jsTask;
+
+// Browsersync Tasks
+function browsersyncServe(callback) {
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+  });
+  callback();
+}
+
+function browsersyncReload(callback) {
+  browsersync.reload();
+  callback();
+}
+
+// Watch Task
+function watchTask() {
+  watch("*.html", browsersyncReload);
+  watch(
+    ["app/scss/**/*.scss", "app/js/**/*.js"],
+    series(scssTask, jsTask, browsersyncReload)
+  );
+}
+
+// Default Gulp task
+exports.default = series(scssTask, jsTask, browsersyncServe, watchTask);
---
+++ Browsersync & Sass & Gulp/README.md
@@ -0,0 +1,4 @@
+# Browsersync + Sass + Gulp in 15 minutes
+
+Available [here](https://www.youtube.com/watch?v=q0E1hbcj-NI).
+
